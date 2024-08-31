af6e10d99e94c4ece2d857f3af330ad0fff654f1
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-08-31 14:42:43 +0530

    Create strong number.java

---
+++ strong number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class armstrong {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number");
+    int num = sc.nextInt();
+
+    int o = num;
+    int sum = 0;
+    while (num > 0) {
+      int digit = num % 10;
+      int fact = 1;
+
+      for (int i = 1; i <= digit; i++) {
+        fact = fact * i;
+      }
+      sum = sum + fact;
+      num /= 10;
+    }
+    if (sum == o) {
+      System.out.println("is strong");
+
+    } else {
+      System.out.println("is not  strong");
+    }
+  }
+}
