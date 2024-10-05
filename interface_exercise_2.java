e29ac5cc5c2b8459e49eecd2ae230d7d6a3619ba
Thokala Charan <166838214+Thokalacharan@users.noreply.github.com>
2024-10-05 15:30:02 +0530

    Update interface_exercise_2.java

--- interface_exercise_2.java
+++ interface_exercise_2.java
@@ -1,3 +1,9 @@
+Exercise 1: Shape Interface
+Objective: Understand interfaces and method implementation.
+• Task: Create an interface Shape with a method getArea(). Implement this interface in
+three classes: Rectangle, Circle, and Triangle. Each class should provide its own
+implementation of the getArea() method.
+
 interface shape {
     double getarea();
 }
@@ -42,5 +48,25 @@ public class main{
        System.out.println("circle = "+ c.getarea());
        System.out.println("rectangle = "+ r.getarea());
        System.out.println("triangle = "+ t.getarea());
+    }
+}
+
+Exercise 2: Animal Interface
+Objective: Explore interface methods and class implementation.
+• Task: Define an interface Animal with a method bark(). Create a class Dog that
+implements this interface and overrides the bark() method to print "Dog is barking".
+
+    interface animal {
+    void bark();
+}
+class dog implements animal {
+    public void bark() {
+        System.out.println("dog is barking");
+    }
+}
+public class main{
+    public static void main(String[] args){
+        dog d = new dog();
+        d.bark();
     }
 }
