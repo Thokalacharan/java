9f3c0aa633ae9a6487b9e35de8f15b702de6dc1a
Thokala Charan <166838214+Thokalacharan@users.noreply.github.com>
2024-10-24 21:47:36 +0530

    Create buildin_exception.java

---
+++ buildin_exception.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class built{
+    public static void main(String[] args){
+        try{
+            int a[] = {1,2,3,4};
+            System.out.println(a[10]);
+            
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
