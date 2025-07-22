835e68ec1504e64aa76ed43f2bdf9ca6f629253c
Thokala Charan <166838214+Thokalacharan@users.noreply.github.com>
2025-07-22 21:02:56 +0530

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the a value");
+        int a = sc.nextInt();
+        System.out.println(a);
+        
+    }
+}
