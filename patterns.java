4c660293c0a04576ef206b4110327461a8c44a74
Thokala Charan <166838214+Thokalacharan@users.noreply.github.com>
2025-09-06 21:49:00 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -1,9 +1,46 @@
-public class pattern {
+i= rows and j= columns
+public class pattern1 {
     public static void main(String[] args) {
        int n=4;
        for(int i=1;i<=n;i++){
          for(int j=1;j<=i;j++){
             System.out.print("*");
+         }
+         System.out.println();
+       }
+    }
+}
+
+public class pattern2 {
+    public static void main(String[] args) {
+       int n=4;
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=n;j++){
+            System.out.print("*");
+         }
+         System.out.println();
+       }
+    }
+}
+
+public class pattern3 {
+    public static void main(String[] args) {
+       int n=4;
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=n-i+1;j++){
+            System.out.print("*");
+         }
+         System.out.println();
+       }
+    }
+}
+
+public class pattern4 {
+    public static void main(String[] args) {
+       int n=4;
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+            System.out.print(j+" ");
          }
          System.out.println();
        }
